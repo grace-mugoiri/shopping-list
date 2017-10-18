e7e07abddff16c8a7bedd98171cf8ea4a22e19c9
gracemugoiri1 <gracemugoiri@gmail.com>
2017-10-18 21:50:59 +0300

    added tests directory

---
+++ shopping-list/tests/test_main.py
@@ -0,0 +1,14 @@
+"""import the necessary modules"""
+import unittest
+import shopping-list
+
+#defining class for the test
+class UnitTestPythonExample(unittest.TestCase):
+    """index function defined"""
+    def test_index(self):
+        return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
