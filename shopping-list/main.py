63e09c3f989008a63083ee38bdb8a6fd50e88e85
gracemugoiri1 <gracemugoiri@gmail.com>
2017-10-28 21:17:11 +0300

    modified main

--- shopping-list/main.py
+++ shopping-list/main.py
@@ -1,60 +1,67 @@
 """import the necessary modules"""
 import os
+import hashlib
 from flask import Flask, render_template, redirect, request, session, url_for, flash
-from wtforms import Form, TextField, validators
+from wtforms import Form, TextField, validators, PasswordField
 
 app = Flask(__name__)
 app.config.from_object("config")
 
 class RegistrationForm(Form):
-    """class method for registration form"""
-    firstName = TextField("First Name:", validators=[validators.required()])
-    secondName = TextField("Second Name:", validators=[validators.required()])
-    email = TextField("e-mail:", validators=[validators.required(), validators.Length(min=6, max=35)])
-    password = TextField('Generate Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
-
+    """registration class defined"""
+    username = TextField('Username:', validators=[validators.required()])
+    email = TextField('Email:', validators=[validators.required(), \
+            validators.Length(min=6, max=35)])
+    password = TextField('Password:', validators=[validators.required(), \
+    validators.Length(min=3, max=35)]) 
 @app.route("/", methods=["GET", "POST"])
 def index():
     """define the index function"""
     return render_template("index.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """define loggin function"""
-    if request.form["password"] == "password" and request.form["username"] == "admin":
-        session["logged_in"] = True
+    """define login function"""
+    if request.method == "POST":
+        print session["username"]        
+        if "username" in session and "password" in session:
+            return redirect(url_for("shopping_list"))
+        else:
+            return render_template("login")
     else:
-        flash("wrong password!")
-    return render_template("login.html")
+        return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """define register function"""
-    form = RegistrationForm(request.form)
-    print form.errors
-    if request.method == "POST":
-        firstName = request.form["firstName"]
-        secondName = request.form["secondName"]
-        email = request.form["email"]
-        password = request.form["password"]
-        print firstName, " ", email, " ", password, " "
+    # form = RegistrationForm(request.form)
+    if request.method != "POST":
+        return render_template("register.html")
+    
+    username = request.form['username']
+    password = request.form['password']
 
-        if form.validate():
-            flash("Thanks for registration ", + firstName + secondName)
-        else:
-            flash("Error: All the fileds arerequired. ")
-    return render_template("register.html", form=form)
+    session["username"] = username
+
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    session["password"] = hashed_password
+
+    return redirect(url_for("login"))
+
 
 @app.route("/shopping_list", methods=["GET", "POST"])
 def shopping_list():
     """define shopping list function"""
     return render_template("shopping_list.html")
 
+
 @app.route("/logout",)
 def loggout():
     """define loggout function"""
     session["logged_in"] = False
     return redirect(url_for("login.html"))
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
